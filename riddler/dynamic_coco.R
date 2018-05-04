e6e0406f8752088c8bed65bbfd5f7e9c3b0f5704
Peter Wendel <pwendel3@gmail.com>
2018-05-04 15:15:48 -0500

    final coco
    coco is done

--- riddler/dynamic_coco.R
+++ riddler/dynamic_coco.R
@@ -1,20 +1,16 @@
-# set pirate count
-pc<-7
-# set how much the monkey takes
-mt<-1
-print(paste('Pirates: ',pc))
-print(paste('Monkey keeps: ',mt ))
+library(plotly)
+library(tidyverse)
+# inverse steal helper functin
+invsteal<-function(x,pc,mt){(pc/(pc-1))*(x+mt)}
 
-invsteal<-function(x){(pc/(pc-1))*(x+mt)}
-
-checknum<-function(xin){
+# function to check if number is a solution
+checknum<-function(xin,pc,mt){
   x<-pc*xin
   solved<-0
   for(i in 1:pc){
-    x<-invsteal(x)
+    x<-invsteal(x,pc,mt)
     if(x!=round(x)){break}
     if(i==pc){
-      print(paste(x,' is a solution!'))
       solved<-x
     }
     
@@ -22,28 +18,83 @@ checknum<-function(xin){
   return(solved)
 }
 
-out<-0
-y<-1
-while(out==0){
-  y<-y+1
-  out<-checknum(y)
+# steal helper function
+steal<-function(x,pc,mt){((pc-1)/pc)*x-mt}
+
+#total function
+# pc-pirate count
+# mt-monkey take count
+# printer- print interesting output
+
+dynacoco<-function(pc,mt,printer=FALSE){
+  if(printer){
+    print(paste('Pirates: ',pc))
+    print(paste('Monkey keeps: ',mt ))
+  }
+  out<-0
+  x<-1
+  
+  # increment until a solution is found
+  while(out==0){
+    out<-checknum(x,pc,mt)
+    x<-x+1
+  }
+
+  # print breakdown if printer is true
+  if(printer){
+    if(printer){print(paste(out,' is a solution!'))}
+    out2<-out
+    keeps<-rep(0,pc)
+    for(i in 1:pc){
+      print(paste(out2,' at start of round ',i))
+      keep<-out2/pc
+      leave<-out2*(pc-1)/pc
+      print(paste('pirate takes ',keep,' and leaves ',leave))
+      out2<-steal(out2,pc,mt)
+      print(paste('monkey gets ',mt, ', ',out2,' left'))
+      keeps[i]<-keep
+      
+    }
+    keeps<-keeps+out2/pc
+    for(i in 1:pc){
+      print(paste('pirate 1 keeps ',keeps[i],' coconuts'))
+    }
+    print(paste('monkey keeps ',mt*pc,' coconuts'))
+  }
+  return(out)
 }
 
-steal<-function(x){((pc-1)/pc)*x-mt}
-out2<-out
-keeps<-rep(0,pc)
-for(i in 1:pc){
-  print(paste(out2,' at start of round ',i))
-  keep<-out2/pc
-  leave<-out2*(pc-1)/pc
-  print(paste('pirate takes ',keep,' and leaves ',leave))
-  out2<-steal(out2)
-  print(paste('monkey gets ',mt, ', ',out2,' left'))
-  keeps[i]<-keep
-  
+# set pirate count
+pcin<-7
+# set how much the monkey takes
+mtin<-1
+
+
+dynacoco(pc=pcin,mt=mtin,printer=TRUE)
+
+#empty matrix
+surftib<-tibble(pirates=numeric(),monkey_take=numeric(),min_coconuts=numeric())
+
+# simulate for 2:10 pirates, 1:10 monkeys
+# (WARNING: this takes a long time)
+for(i in 2:8){
+  for(j in 1:8){
+    coco<-dynacoco(i,j)
+    surftib<-surftib%>%add_row(pirates=i,monkey_take=j,min_coconuts=coco)
+
+  }
 }
-keeps<-keeps+out2/7
-for(i in 1:pc){
-  print(paste('pirate 1 keeps ',keeps[i],' coconuts'))
-}
-print(paste('monkey keeps ',mt*pc,' coconuts'))
+surftib$pirates
+surftib$pirates<-rep(2:8,each=8)
+View(surftib)
+bigtib<-surftib%>%spread(monkey_take,min_coconuts)
+tibmat<-as.matrix(bigtib[,2:ncol(bigtib)])
+
+p<-plot_ly(data=surftib,x=1:8,y=2:8,z=~tibmat)%>%
+  add_surface()%>%layout(title='Min. Coconuts by Pirate, Monkey Take',
+                               scene=list(xaxis=list(title='Monkey Takes'),
+                                          yaxis=list(title='Pirates'),
+                                          zaxis=list(title='Min. Coconuts'))
+  )
+
+p
