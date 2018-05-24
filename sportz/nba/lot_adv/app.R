bc1aad749f614c9b4f186af753d0364f3781baab
Peter Wendel <pwendel3@gmail.com>
2018-05-24 17:47:27 -0500

    wfh
    wfh work

---
+++ sportz/nba/lot_adv/app.R
@@ -0,0 +1,108 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(plotly)
+#library(ggplotly)
+#setwd('C:/Users/pwendel/Documents/GitHub/DSGit/sportz/nba')
+alldat<-read_csv('lot_adv.csv')
+colnames(alldat)<-str_replace(colnames(alldat),'%','Percentage')
+colnames(alldat)<-str_replace(colnames(alldat),'/','Per')
+colnames(alldat)<-str_replace(colnames(alldat),'3','Three')
+colnames(alldat)<-str_replace(colnames(alldat),'2','Two')
+
+
+alldat%>%mutate_if(is.numeric,as.double)
+# Define UI for application that draws a histogram
+ui <- fillPage(
+  
+  # Application title
+  titlePanel("Advanced Metrics by Draft Choice"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(div(style="overflow-y: scroll"),width=3,
+      selectInput("var",
+                  "Advanced Metric:",
+                 choices=colnames(alldat)[31:52],
+                 selected='VORP'),
+    
+    sliderInput("drafts",
+                "Drafts:",
+                min = 1985,
+                max = 2017,
+                step=1,sep='',
+                value = c(1985,2017)),
+    
+  
+  sliderInput("exp",
+              "Years Experience:",
+              min = 1,
+              max = 5,
+              step=1,sep='',
+              value = c(1,1)),
+  sliderInput("min",
+              "Years Experience:",
+              min = 1,
+              max = 5,
+              step=1,sep='',
+              value = c(1,1)),
+  
+  checkboxGroupInput('pos','Position Filter:',
+              sort(alldat$Pos_season%>%unique()),
+              selected=sort(alldat$Pos_season%>%unique())),
+              
+  sliderInput("minmin",
+              "Minimum Minutes in Season:",
+              min = 0,
+              max = 4000,
+              step=100,sep='',
+              value = 500)
+              
+              )
+  ,
+  
+    
+  mainPanel(
+    plotlyOutput("advplot",height='100%'),height='1000px'
+  )
+
+# Show a plot of the generated distribution
+
+)
+
+)
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  plotdat<- reactive({alldat%>%filter(Year_draft>=input$drafts[1] & Year_draft<=input$drafts[2]&
+                              sincedrafted>=input$exp[1] & sincedrafted<=input$exp[2]&
+                                Pos_season%in%input$pos & MP_season>=input$minmin[1])}) 
+  #
+  output$advplot <- renderPlotly({
+    # generate bins based on input$bins from ui.R
+    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    p<-ggplot(data=plotdat())+aes_string(y=input$var)+
+      aes(x=Pk,color=Pos_season,text=sprintf('Player: %s<br>%.f Team: %s<br>Age: %.f<br>%.f, Pick %.f'
+                                                  ,Player_season,Year_season,Tm_season,Age_season,Year_draft,Pk))+
+      #geom_smooth(na.rm=TRUE,se=FALSE,size=1,linetype='dashed')+
+      geom_jitter(width=0.225)+
+      facet_grid(sincedrafted~.)+
+      geom_smooth(aes_string(x='Pk',y=input$var),na.rm=TRUE,se=FALSE,size=1,linetype='dashed',col='black',inherit.aes=FALSE)
+    # draw the histogram with the specified number of bins
+    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    ggplotly(p,tooltip='text')
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
