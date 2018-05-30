e116680fedd3faf76554fcf6cd793365ca1c0e80
Peter Wendel <pwendel3@gmail.com>
2018-05-30 18:06:34 -0500

    multiplot
    doin it big

--- sportz/nba/lot_adv/app.R
+++ sportz/nba/lot_adv/app.R
@@ -12,7 +12,7 @@ library(ggplot2)
 library(tidyverse)
 library(plotly)
 #library(ggplotly)
-#setwd('C:/Users/pwendel/Documents/GitHub/DSGit/sportz/nba')
+#setwd('C:/Users/pwend/Documents/GitHub/DSGit/sportz/nba')
 alldat<-read_csv('lot_adv.csv')
 colnames(alldat)<-str_replace(colnames(alldat),'%','Percentage')
 colnames(alldat)<-str_replace(colnames(alldat),'/','Per')
@@ -27,18 +27,19 @@ colnames(alldat)<-str_replace(colnames(alldat),'2','Two')
 # }
 
 alldat%>%mutate_if(is.numeric,as.double)
+
 # Define UI for application that draws a histogram
-ui <- fillPage(
+ui <- fluidPage(div(style="overflow-y:scroll"),
   
   # Application title
   titlePanel("Advanced Metrics by Draft Choice"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
-    sidebarPanel(div(style="overflow-y: scroll"),width=3,
+    sidebarPanel(width=3,
       selectInput("var",
                   "Advanced Metric:",
-                  label = labelcol,
+                  
                  choices=colnames(alldat)[31:52],
                  selected='VORP'),
     
@@ -56,16 +57,16 @@ ui <- fillPage(
               max = 5,
               step=1,sep='',
               value = c(1,1)),
-  sliderInput("min",
-              "Years Experience:",
-              min = 1,
-              max = 5,
-              step=1,sep='',
-              value = c(1,1)),
+  # sliderInput("min",
+  #             "Years Experience:",
+  #             min = 1,
+  #             max = 5,
+  #             step=1,sep='',
+  #             value = c(1,1)),
   
   checkboxGroupInput('pos','Position Filter:',
-              sort(alldat$Pos_season%>%unique()),
-              selected=sort(alldat$Pos_season%>%unique())),
+                     sort(alldat$Pos_season%>%unique()),
+                     selected=alldat$Pos_season%>%unique()),
               
   sliderInput("minmin",
               "Minimum Minutes in Season:",
@@ -73,13 +74,13 @@ ui <- fillPage(
               max = 4000,
               step=100,sep='',
               value = 500)
-              
-              )
+  
+    )
   ,
   
     
-  mainPanel(
-    plotlyOutput("advplot",height='100%'),height='1000px'
+  mainPanel(div(style="overflow-y:scroll"),
+    plotlyOutput("advplot")
   )
 
 # Show a plot of the generated distribution
@@ -87,25 +88,29 @@ ui <- fillPage(
 )
 
 )
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   plotdat<- reactive({alldat%>%filter(Year_draft>=input$drafts[1] & Year_draft<=input$drafts[2]&
-                              sincedrafted>=input$exp[1] & sincedrafted<=input$exp[2]&
-                                Pos_season%in%input$pos & MP_season>=input$minmin[1])}) 
+                                        sincedrafted>=input$exp[1] & sincedrafted<=input$exp[2] &
+                                        Pos_season%in%input$pos & MP_season>=input$minmin)}) 
+  
   #
+ 
   output$advplot <- renderPlotly({
-    # generate bins based on input$bins from ui.R
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    print('through here')
     p<-ggplot(data=plotdat())+aes_string(y=input$var)+
       aes(x=Pk,color=Pos_season,text=sprintf('Player: %s<br>%.f Team: %s<br>Age: %.f<br>%.f, Pick %.f'
-                                                  ,Player_season,Year_season,Tm_season,Age_season,Year_draft,Pk))+
+                                                 ,Player_season,Year_season,Tm_season,Age_season,Year_draft,Pk))+
       #geom_smooth(na.rm=TRUE,se=FALSE,size=1,linetype='dashed')+
       geom_jitter(width=0.225)+
       facet_grid(sincedrafted~.)+
       geom_smooth(aes_string(x='Pk',y=input$var),na.rm=TRUE,se=FALSE,size=1,linetype='dashed',col='black',inherit.aes=FALSE)
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    ggplotly(p,tooltip='text')
+   p<-ggplotly(p,tooltip='text',height=500*(input$exp[2]-input$exp[1]+1))
+  #p<-plotly_build(p)
+  #heightin<-reactive({paste0(as.character(*500),'px')})
+  #p$height=()
+    
   })
   
 }
