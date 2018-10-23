65a89dfb310c0f53dd8e3d0e2ecb8f8d8057b595
Peter Wendel <pwendel3@gmail.com>
2018-10-23 15:41:13 -0500

    push practice
    push practice

--- sportz/nfl/pre_matter.R
+++ sportz/nfl/pre_matter.R
@@ -3,9 +3,14 @@ library(tidyverse)
 library(plotly)
 library(psych)
 
-years<-2017:1983
+setwd('C:/Users/pwend/Documents/GitHub/DSGit/sportz/nfl')
+
+alltables<-read_csv('NFL-preseason-regseason.csv')
+
+years<-2017:1982
 for(j in 1:length(years)){
   year<-years[j]
+  year<-1982
   baseurl<-paste0('https://www.pro-football-reference.com/years/',year,'/')
   preurl<-paste0(baseurl,'preseason.htm')
   
@@ -34,22 +39,25 @@ for(j in 1:length(years)){
   psb<-gsub("[^A-Za-z0-9 ]","",psb)
   
   basetable%>%mutate(playoffs=ifelse(Tm==psb,'champion',playoffs))->basetable
-  
-  prehtm<-read_html(preurl)
-  pretables<-prehtm%>%html_table()
-  
-  for(i in 1:2){
-    if(i==1){
-      pretable<-pretables[i]
+  if(year!=1982){
+    prehtm<-read_html(preurl)
+    pretables<-prehtm%>%html_table()
+    
+    for(i in 1:2){
+      if(i==1){
+        pretable<-pretables[i]
+      }
+      else{
+        pretable<-bind_rows(pretable,pretables[i])
+      }
     }
-    else{
-      pretable<-bind_rows(pretable,pretables[i])
-    }
+    
+    pretable<-pretable%>%filter(!grepl('[A-Za-z]',W))%>%mutate(Tm=gsub("[^A-Za-z0-9 .]","",Tm))
+    
+    alltable<-basetable%>%inner_join(pretable,by='Tm',suffix=c('_reg','_pre'))
   }
+  else{alltable<-basetable}
   
-  pretable<-pretable%>%filter(!grepl('[A-Za-z]',W))%>%mutate(Tm=gsub("[^A-Za-z0-9 .]","",Tm))
-  
-  alltable<-basetable%>%inner_join(pretable,by='Tm',suffix=c('_reg','_pre'))
   if(j==1){
     alltables<-alltable
   }
@@ -66,29 +74,75 @@ alltables%>%mutate_at(numer,as.numeric)->alltables
 alltables%>%mutate(pre_win_p=W_pre/(W_pre+L_pre))%>%rename(reg_win_p=`W-L%`)->alltables
 
 
-p<-alltables%>%ggplot(aes(x=pre_win_p,y=reg_win_p,col=playoffs,
+breakers<-c(-1,0.438,0.625,1.2)
+labelers<-paste(c('0-7 Wins','8-10 Wins','11-16 Wins'),' Last Year')
+#labs<-factor(1:4)
+
+p<-alltables%>%mutate(lag_reg_win_p=cut(lag_reg_win_p,breaks=breakers,label=labelers))%>%filter(!is.na(lag_reg_win_p))%>%
+  ggplot(aes(x=pre_win_p,y=reg_win_p,col=playoffs,
                           text=paste0(year,' ',Tm,'<br>Reg:',W_reg,'-',L_reg,'<br>Pre:',W_pre,'-',L_pre)))+geom_jitter(width=.03,height=0.03)+
   xlab('Preseason Win %')+ylab('Regular Season Win %')+ggtitle('Regular Season Win % vs. Preseason Win %')+
-  labs(color='Playoffs',subtitle='1983-2017')
+  #geom_smooth(method='glm',aes(x=pre_win_p,y=reg_win_p),na.rm=TRUE,se=FALSE,size=1,linetype='dashed',col='black',inherit.aes=FALSE)+
+  labs(color='Playoffs',subtitle='1983-2017')+facet_wrap(vars(lag_reg_win_p))
 
+p2<-alltables%>%mutate(lag_reg_win_p=cut(lag_reg_win_p,breaks=breakers,,labels=labelers))%>%filter(!is.na(lag_reg_win_p))%>%ggplot(aes(x=pre_win_p,y=reg_win_p,col=playoffs,
+  text=paste0(year,' ',Tm,'<br>Reg:',W_reg,'-',L_reg,'<br>Pre:',W_pre,'-',L_pre)))+geom_jitter(width=.03,height=0.03)+
+  xlab('Preseason Win %')+ylab('Regular Season Win %')+ggtitle('Regular Season Win % vs. Preseason Win %')+
+  #geom_smooth(method='glm',aes(x=pre_win_p,y=reg_win_p),na.rm=TRUE,se=FALSE,size=1,linetype='dashed',col='black',inherit.aes=FALSE)+
+  labs(color='Playoffs',subtitle='1983-2017')#+facet_wrap(vars(improved),scales='free')
+
+
+p
 ggplotly(p,tooltip='text')
+
+alltables$improved<-alltables$reg_win_p>alltables$lag_reg_win_p
+
+alltables$improved%>%hist()
 
 alltables%>%ggplot(aes=playoffs)
 
-pdist<-alltables%>%mutate(pre_win_p=cut(pre_win_p,breaks=c(-1,0.2,0.4,0.6,0.8,1.2),labels=c('0-20%','20-40%','40-60%','60-80%','80-100%')))%>%group_by(pre_win_p,playoffs)%>%summarise(count=n())%>%
-  spread(playoffs,count)%>%mutate_at(2:4,~replace_na(.,0))%>%mutate(
+quantile(alltables$lag_reg_win_p,c(0.3333,0.666,0.75),na.rm=TRUE)
+
+quantile(alltables$pre_win_p,c(0.3333,0.666),na.rm=TRUE)
+
+pdistbd<-alltables%>%mutate(pre_win_p=cut(pre_win_p,breaks=c(-1,0.4,0.6,1.2),labels=c('0-40%','41-60%','61-100%')),
+                          lag_reg_win_p=cut(lag_reg_win_p,breaks=breakers,labels=labelers)) %>%
+  group_by(pre_win_p,lag_reg_win_p,playoffs)%>%summarise(count=n())%>%
+  spread(playoffs,count)%>%mutate_at(3:5,~replace_na(.,0))%>%mutate(
                                                   total=champion+no+yes,
                                                   champion=champion/total,
                                                   no=no/total,
                                                   yes=yes/total)
 
-pdist%>%mutate_at(c('champion','no','yes'),~.*total)
-pdist
 
-pd<-pdist%>%filter(total>50)%>%gather(outcome,dist,-pre_win_p,-total)%>%mutate(outcome=ifelse(outcome=='yes','playoffs',outcome))%>%ggplot(aes(x=pre_win_p,y=dist,fill=outcome,text=paste0('Outcome Probability:',dist,'<br>Total Cases:',total)))+
-  geom_col(position='dodge')+xlab('Preseason Win %')+ylab('Probability')+ggtitle('Season Outcome Probability by Preseason Outcome')
+pd<-pdistbd%>%gather(outcome,dist,-pre_win_p,-total,-lag_reg_win_p)%>%mutate(outcome=ifelse(outcome=='yes','playoffs',outcome))%>%filter(!is.na(lag_reg_win_p))%>%
+  ggplot(aes(x=pre_win_p,y=dist,fill=outcome,text=paste0('Outcome Probability:',dist,'<br>Total Cases:',total)))+
+  geom_col(position='dodge')+xlab('Preseason Win %')+ylab('Probability')+ggtitle('Season Outcome Probability by Preseason Outcome')+facet_wrap(vars(lag_reg_win_p))
 
 ggplotly(pd,tooltip='text')
+
+
+
+
+pdist<-alltables%>%mutate(pre_win_p=cut(pre_win_p,breaks=c(-1,0.4,0.6,1.2),labels=c('0-40%','41-60%','61-100%')),
+                            lag_reg_win_p=cut(lag_reg_win_p,breaks=breakers,labels=labelers))%>%
+  group_by(pre_win_p,playoffs)%>%summarise(count=n())%>%
+  spread(playoffs,count)%>%mutate_at(2:4,~replace_na(.,0))%>%mutate(
+    total=champion+no+yes,
+    champion=champion/total,
+    no=no/total,
+    yes=yes/total)
+
+
+pd2<-pdist%>%gather(outcome,dist,-pre_win_p,-total)%>%mutate(outcome=ifelse(outcome=='yes','playoffs',outcome))%>%ggplot(aes(x=pre_win_p,y=dist,fill=outcome,text=paste0('Outcome Probability:',dist,'<br>Total Cases:',total)))+
+  geom_col(position='dodge')+xlab('Preseason Win %')+ylab('Probability')+ggtitle('Season Outcome Probability by Preseason Outcome')#+facet_wrap(vars(lag_reg_win_p))
+
+
+
+ggplotly(pd2
+         ,tooltip='text')
+
+
 
 corrr<-corr.test(alltables$reg_win_p,alltables$pre_win_p,method='spearman')
 
@@ -108,7 +162,8 @@ alltables%>%arrange(Tm,year)%>%group_by(Tm)%>%
 lmer<-lm(reg_win_p~lag_reg_win_p+pre_win_p,data=alltables)
 summary(lmer)
 
-alltables%>%ggplot(aes(x=pre_win_p,y=lag_reg_win_p,col=reg_win_p))+geom_point()
+alltables%>%ggplot(aes(x=pre_win_p,y=lag_reg_win_p,col=reg_win_p))+geom_point()+
+  facet_wrap(vars(lag_reg_win_p),,scales='free')
 
                    
 
@@ -116,3 +171,17 @@ plot_ly(alltables,z=~reg_win_p,y=~lag_reg_win_p,x=~pre_win_p,
         color=~playoffs)
 
 write_csv(alltables,path='NFL-preseason-regseason.csv')
+
+View(alltables)
+
+#breaker<-c(-1,0.25,0.5,0.75,1.2)
+
+for(i in 1:3){
+  alltables%>%filter(lag_reg_win_p>breakers[i]&lag_reg_win_p<breakers[i+1])->temptab
+  print(corr.test(temptab$reg_win_p,temptab$pre_win_p))
+  print(summary(lm(reg_win_p~pre_win_p,data=temptab)))
+}
+
+
+print(summary(lm(reg_win_p~pre_win_p,data=alltables)))
+
